32fe9edb97765f51fafb395098f185110e85f2b8
Thoobi <sydneydaniel76@gmail.com>
2024-10-03 15:00:47 +0100

    adds the 3-all an 3-main file

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function handleProfileSignup() {
+  try {
+    const photoResponse = await uploadPhoto('photo-profile-1');
+    const userResponse = await createUser('Guillaume', 'Salva');
+
+    console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
+  } catch (error) {
+    throw Error('Signup system offline');
+  }
+}

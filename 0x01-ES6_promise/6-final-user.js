87cdfafd7b8b7ba50cd85eda21dddb1da1590eb9
Thoobi <sydneydaniel76@gmail.com>
2024-10-03 16:25:27 +0100

    fixex

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,19 +2,17 @@ import uploadPhoto from './5-photo-reject';
 import signUpUser from './4-user-promise';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
+
   const promises = [
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ];
 
-  // Use Promise.allSettled to handle all promises
   const results = await Promise.allSettled(promises);
 
-  // Map the results to the desired structure
-  return results.map((result) => ([
-    {
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    },
-  ]));
+  // Return results formatted with status and value/reason
+  return results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason
+  }));
 }

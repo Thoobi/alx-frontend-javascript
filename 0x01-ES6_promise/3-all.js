a05642bace0ea9633ebfbb2f316dcb5cf25a3739
Thoobi <sydneydaniel76@gmail.com>
2024-10-03 15:32:57 +0100

    please just check

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,8 +3,9 @@ import { uploadPhoto, createUser } from './utils';
 export default async function handleProfileSignup() {
   try {
     const [photoResponse, userResponse] = await Promise.all([
-      uploadPhoto(),
-      createUser(),
+      uploadPhoto('photo-profile-1'),
+      createUser('Guillaume'),
+      createUser('Salva'),
     ]);
 
     console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);

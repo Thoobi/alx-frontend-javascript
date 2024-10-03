b9e7b6e819c228e9ee3da9d612d98d2f7a433c7f
Thoobi <sydneydaniel76@gmail.com>
2024-10-03 16:15:17 +0100

    adds the 6-final-user an 6-main

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,18 @@
+import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ];
+
+  // Use Promise.allSettled to handle all promises
+  const results = await Promise.allSettled(promises);
+
+  // Map the results to the desired structure
+  return results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  }));
+}

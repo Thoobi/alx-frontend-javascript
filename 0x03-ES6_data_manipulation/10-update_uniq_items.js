893ca535a86475b5852191644ee630432aff152d
Thoobi <sydneydaniel76@gmail.com>
2024-10-08 13:22:08 +0100

    adds the 10-main file *3

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,19 @@
-const updateUniqueItems = (map) => {
-  if (!(map instanceof Map)) throw new Error('Cannot process');
+function updateUniqueItems(map) {
+  // Check if the argument is a Map
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
 
-  map.forEach((value, key) => {
-    if (value === 1) map.set(key, 100);
-  });
-};
+  // Iterate through the map entries
+  for (const [item, quantity] of map.entries()) {
+    // Update quantity if it's 1
+    if (quantity === 1) {
+      map.set(item, 100);
+    }
+  }
+
+  // Return the updated map
+  return map;
+}
 
 export default updateUniqueItems;
